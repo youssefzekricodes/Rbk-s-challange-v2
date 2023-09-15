e9aa965ed35be5f0621fb501e1edb499d3584a0a
youssefzekri <youssef.essedik.zekri@gmail.com>
2023-09-15 22:58:42 +0100

    set up header foncionality

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,10 @@
 import { ReactElement } from "react";
+import Header from "./components/Header/Header";
 
 
 export default function Layout({children} :{ children: ReactElement | ReactElement[];}) {
-  return <div>{children}</div>;
+  return <div className="Layout">
+    <Header/>
+    {children}
+    </div>;
 }
