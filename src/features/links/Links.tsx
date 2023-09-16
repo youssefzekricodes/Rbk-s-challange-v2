971a86eccdc296c58df19bbdecddeac093f893d2
youssefzekri <youssef.essedik.zekri@gmail.com>
2023-09-16 10:14:30 +0100

    set up main ui

--- src/features/links/Links.tsx
+++ src/features/links/Links.tsx
@@ -1,3 +1,14 @@
+import CardHeader from "../../components/CardHeader/CardHeader";
+
 export default function Links() {
-  return <div>Links</div>;
+  return (
+    <div className="Card__content Links">
+      <CardHeader
+        title={"Customize your links"}
+        subtitle={
+          "Add , edit and remove links below and then share all your profiles with the world!"
+        }
+      />
+    </div>
+  );
 }
