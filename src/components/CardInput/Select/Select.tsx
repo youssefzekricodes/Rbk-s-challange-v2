4d44b7ffb8cd900f35866b7b5b0504c6f27bf1f6
youssefzekri <youssef.essedik.zekri@gmail.com>
2023-09-16 12:45:41 +0100

    improving input style

--- src/components/CardInput/Select/Select.tsx
+++ src/components/CardInput/Select/Select.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as DownArrow } from "../../../assets/icons/downarrow.svg";
+import { ReactComponent as Github } from "../../../assets/icons/github.svg";
+
+function SelectOption({
+  Icon,
+  name,
+  action,
+}: {
+  Icon: any;
+  name: string;
+  action: any;
+}) {
+  return (
+    <div className="Select__Options__option" onClick={() => action("GitHub")}>
+
+      <p>{name}</p>
+    </div>
+  );
+}
 export default function Select() {
+  const [showSelect, setShowSelect] = useState(true);
+  const [selectedOption, setSelectedOption] = useState("GitHub");
   return (
     <div className="Select">
       <div className="Select__selectedOption">
-         <p>hello</p> <DownArrow />
+        <p>hello</p> <DownArrow onClick={() => setShowSelect(!showSelect)} />
       </div>
-      <div className="Select__Options">
-        <div className="Select__Options__option">hello</div>
-      </div>
+      {showSelect ? (
+        <div className="Select__Options">
+          <SelectOption
+            Icon={<Github />}
+            name={"Github"}
+            action={setSelectedOption}
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
